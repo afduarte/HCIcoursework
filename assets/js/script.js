4b81d3de381fe5433cf650e303e9677fc36a18db
afduarte <a.fduarte1@gmail.com>
2016-04-22 22:16:43 +0100

    Modals working, look shitty

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,26 @@
 $(function() {
   var imgBasePath = 'assets/img/';
-  
+
   // Object holding the available stuff
-  var stuffAvailable = {
+  stuffAvailable = {
     bg: ["bg/1.jpg","bg/2.jpg"],
     models: [
       {
         img: 'models/guy.png',
-        shirts: [],
+        shirts: [
+          {
+            shirt1:{
+              path: 'testpath',
+              colours: ['red','blue','black']
+            }
+          },
+          {
+            shirt2:{
+              path: 'testpath2',
+              colours: ['red','blue','black']
+            }
+          }
+        ],
         trousers: []
 
       },
@@ -19,21 +32,23 @@ $(function() {
       }
     ]
   };
-  
-    // var to hold the initial state and keep track of the changes
-  var state = {
+
+  // var to hold the initial state and keep track of the changes
+  state = {
     bg: {
       img:stuffAvailable.bg[0]
     },
     model: stuffAvailable.models[0]
   };
-  
+
+  //  var state = window.state;
+
   // Load BG
   $('.main-frame img#bg').attr('src',imgBasePath+state.bg.img);
-  
+
   // Super complicated series of functions to load the initial bg and model that takes care of javascript's asynchronous behaviour
   $('.main-frame img#bg').load(function(){
-    
+
     // Store the height of the bg img, cause we'll use it later
     state.bg.height = parseFloat($('.main-frame img#bg').css('height'));
     // On BG load, load the model
@@ -43,10 +58,18 @@ $(function() {
       $('.main-frame img#model').css('margin-top', function(){
         return (state.bg.height/2)-(parseFloat($('.main-frame img#model').css('height'))/2);
       });
-      
+
       $('.clothes-picker').css('height',state.bg.height);
       $('.main-frame img#model').css('display','block');
     });
+  });
+
+  $(document).on("click", ".open-pickerModal", function () {
+    console.log('modal event fired');
+    var myBookId = $(this).data('id');
+    console.log(myBookId);
+    $(".modal-body #bookId").val( myBookId );
+//    $('#pickerModal').modal('show');
   });
 
 });
